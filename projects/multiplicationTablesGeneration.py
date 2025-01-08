7974e47b35e32d139a9d0fab471678d8d9c32bc7
JMCoC <juan.ramirez.agudelo@correounivalle.edu.co>
2025-01-08 12:05:37 -0500

    Review loops and conditional statements, plus a project applying concepts

---
+++ projects/multiplicationTablesGeneration.py
@@ -0,0 +1,22 @@
+def multiplicationTablesGeneration():
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            break
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+
+if __name__ == "__main__":
+    continue_program = "y"
+    while continue_program == "y":
+        multiplicationTablesGeneration()
+        while True:
+            continue_program = input("Do you want to continue? (y/n): ").lower()
+            if continue_program == "y" or continue_program == "n":
+                break
+            else:
+                print("Please enter a valid input.")
+                continue
