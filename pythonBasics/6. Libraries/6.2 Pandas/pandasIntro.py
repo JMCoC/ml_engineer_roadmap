7ced99c609fdb2bebfa8570e01336dd274b279e3
JMCoC <juan.ramirez.agudelo@correounivalle.edu.co>
2025-02-13 21:23:53 -0500

    Pandas Series and its methods done

--- pythonBasics/6. Libraries/6.2 Pandas/pandasIntro.py
+++ pythonBasics/6. Libraries/6.2 Pandas/pandasIntro.py
@@ -25,7 +25,7 @@ data = np.array([1, 2, 3, 4, 5])
 s = pd.Series(data) #A column in a excel sheet
 print(s)
 
-#Creating Pandas Series
+#Creating Pandas Series (Column in Excel Sheet)
 """
 In the real world, a Pandas Series will be created by loading the datasets
 from existing storage, storage can be SQL Database, CSV file, and Excel file.
@@ -91,3 +91,45 @@ print(s[[100, 102, 104]]) #1, 3, 5
 df = pd.read_csv("pythonBasics/CSV/nba.csv")
 ser = pd.Series(df['Name'])
 print(ser[[0, 50, 100]])
+
+#Indexing and Selecting Data in Series (iloc, loc)
+#iloc: iloc is an integer-location based indexing for selection by position.
+#loc: loc is label-based indexing function.
+
+df = pd.read_csv("pythonBasics/CSV/nba.csv")
+ser = pd.Series(df['Name'])
+data = ser.head(10)
+print(data.loc[3:6])
+print(data.iloc[3:6])
+
+#Binary Operation on Series (Addition, Subtraction, Multiplication, and Division, etc.)
+
+data1 = pd.Series([1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
+data2 = pd.Series([10, 20, 30, 40, 50], index=['a', 'b', 'c', 'd', 'e'])
+print(data1.add(data2, fill_value=0))
+print(data1.sub(data2, fill_value=0))
+
+#Converison Operation on Series (to_numpy(), to_list(), to_dict(), astype(), etc.)
+
+data = pd.read_csv("pythonBasics/CSV/nba.csv")
+data.dropna(inplace=True) #Drop all the columns with NaN values
+before = data.dtypes #Before conversion
+data['Salary'] = data['Salary'].astype(int)
+data['Number'] = data['Number'].astype(str)
+after = data.dtypes #After conversion
+
+print(before)
+print(after)
+
+#to_list()
+
+data = pd.read_csv("pythonBasics/CSV/nba.csv")
+data.dropna(inplace=True)
+dtypeBefore = type(data['Salary'])
+salaryList = data['Salary'].to_list()
+dtypeAfter = type(salaryList)
+
+print(dtypeBefore)
+print(dtypeAfter)
+
+print(data.info())
